38221d53222a1545361812845674db5283a50914
ogg130 <ogg130@gmail.com>
2019-12-29 09:18:22 -0700

    Initial Commit

---
+++ BethanysPieShopStockApp/BethanysPieShopStockApp/Model/Pie.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BethanysPieShopStockApp.Model
+{
+    public class Pie : INotifyPropertyChanged
+    {
+        private int _id { get; set; }
+        private string _pieName { get; set; }
+
+        private double _price { get; set; }
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                RaisePropertyChanged(nameof(Id));
+            }
+        }
+
+        public string PieName
+        {
+            get => _pieName;
+            set
+            {
+                _pieName = value;
+                RaisePropertyChanged(nameof(PieName));
+            }
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                RaisePropertyChanged(nameof(Price));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            // If propertyChange is not null, invoke event
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ BethanysPieShopStockApp/Pie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BethanysPieShopStockApp.Model
+{
+    public class Pie
+    {
+        public int Id { get; set; }
+
+        public string PieName { get; set; }
+
+        public double Price { get; set; }
+    }
+}
